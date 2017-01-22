08b99da776aa6a14624245e3046ef81518d0bcef
chiptus <chiptus@gmail.com>
2017-01-22 15:16:07 +0100

    remove ts console.log

--- timestamp-microservice/index.js
+++ timestamp-microservice/index.js
@@ -5,11 +5,9 @@ const { parseDate } = require('./date-utils');
 module.exports = addTimeStampMS;
 
 function addTimeStampMS(app, root) {
-console.log(root);
   app.all(`${root}/:dateStr`, ({ params: { dateStr } }, res) => {
     res.end(JSON.stringify(parseDate(dateStr)));
   });
-
 }
 
 
