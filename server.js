9a503f230a66834ae11ddf3e8292d73324b214c9
chiptus <chiptus@gmail.com>
2017-01-22 15:57:27 +0100

    run req-head-parse from root

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
+require('./request-header-parser')(app, '/req-head-parser');
+
 require('./timestamp-microservice/')(app,'/timestamp-microservice');
 
 app.listen(port, function () {
